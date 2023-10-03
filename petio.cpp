5ff0e5dc126c074e1644691ec3c06adfa6888fba
steve <jscrane@gmail.com>
2023-10-03 14:01:43 +0100

    merge prg.cpp into petio.cpp

--- petio.cpp
+++ petio.cpp
@@ -153,3 +153,33 @@ void petio::sound_octave(uint8_t o) {
 	_octave = o;
 	sound_on();
 }
+
+bool petio::load_prg()
+{
+	if (!files.more())
+		return false;
+
+	uint8_t lo = files.read();
+	uint8_t hi = files.read();
+	Memory::address a = lo + (hi << 8);
+	while (files.more())
+		memory[a++] = files.read();
+	
+	// based on mem_get/set_basic_text() from vice/petmem.c
+	memory[0xc7] = memory[0x28];
+	memory[0xc8] = memory[0x29];
+
+	lo = (uint8_t)(a & 0xff);
+	memory[0x2a] = lo;
+	memory[0x2c] = lo;
+	memory[0x2e] = lo;
+	memory[0xc9] = lo;
+
+	hi = (uint8_t)(a >> 8);
+	memory[0x2b] = hi;
+	memory[0x2d] = hi;
+	memory[0x2f] = hi;
+	memory[0xca] = hi;
+	
+	return true;
+}
