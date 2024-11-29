25c2c460abf44f942e98da6ed8b73700f4ef21f8
steve <jscrane@gmail.com>
2024-11-29 14:30:33 +0000

    ...

--- sound.h
+++ sound.h
@@ -2,6 +2,10 @@
 
 // https://www.zimmers.net/cbmpics/cbm/PETx/petfaq.html
 // see: HOW DO I MAKE SOUND ON MY PET?
+// poke 59464, freq (or 0 for no sound): 0xe848 = VIA T2LO
+// poke 59466, octave: 0xe84a = VIA SR
+// poke 59467, 16=on 0=off: 0xe84b = VIA ACR
+// also: http://blog.tynemouthsoftware.co.uk/2022/05/pet-sounds.html
 class sound {
 public:
 	sound(): _octave(0), _freq(0) {}
